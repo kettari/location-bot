89fd4fb0f565d56ebc50181f454af394c729b6a8
Andy <kettari@gmail.com>
2025-04-20 22:41:39 +0300

    #3 improved fetching algorithm, excluded ChatGPT

--- internal/entity/game.go
+++ internal/entity/game.go
@@ -15,22 +15,22 @@ const (
 
 type Game struct {
 	gorm.Model
-	ExternalID       string            `json:"id" gorm:"unique;not null"`
-	Joinable         bool              `json:"joinable" gorm:"default:false;not null"`
-	URL              string            `json:"url" gorm:"size:1024"`
-	Title            string            `json:"title" gorm:"size:1024"`
-	Date             time.Time         `json:"date"`
-	Setting          string            `json:"setting" gorm:"size:100"`
-	System           string            `json:"system" gorm:"size:100"`
-	Genre            string            `json:"genre" gorm:"size:100"`
-	MasterName       string            `json:"master_name" gorm:"size:100"`
-	MasterLink       string            `json:"master_link" gorm:"size:1024"`
-	Description      string            `json:"description"`
-	Notes            string            `json:"notes"`
-	SeatsTotal       int               `json:"seats_total" gorm:"default:0;not null"`
-	SeatsFree        int               `json:"seats_free" gorm:"default:0;not null"`
-	NotificationSent bool              `json:"-" gorm:"default:false"`
-	CalendarEvent    CalendarEventType `json:"-" gorm:"-:all"`
+	ExternalID       string    `json:"id" gorm:"unique;not null"`
+	Joinable         bool      `json:"joinable" gorm:"default:false;not null"`
+	URL              string    `json:"url" gorm:"size:1024"`
+	Title            string    `json:"title" gorm:"size:1024"`
+	Date             time.Time `json:"date"`
+	Setting          string    `json:"setting" gorm:"size:100"`
+	System           string    `json:"system" gorm:"size:100"`
+	Genre            string    `json:"genre" gorm:"size:100"`
+	MasterName       string    `json:"master_name" gorm:"size:100"`
+	MasterLink       string    `json:"master_link" gorm:"size:1024"`
+	Description      string    `json:"description"`
+	Notes            string    `json:"notes"`
+	SeatsTotal       int       `json:"seats_total" gorm:"default:0;not null"`
+	SeatsFree        int       `json:"seats_free" gorm:"default:0;not null"`
+	NotificationSent bool      `json:"-" gorm:"default:false"`
+	Slot             int       `json:"-" gorm:"-:all"`
 }
 
 func (g *Game) Equal(game *Game) bool {
